a6eb7ac2857bd5c772ee64c34f1e90db75ae9a50
Andreas Grech <agnt666@gmail.com>
2010-12-22 16:49:06 +0100

    Initial Commit

---
+++ XNASingleStroke/KeyDownEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+
+namespace XNASingleStroke
+{
+    public class KeyDownEventArgs:EventArgs
+    {
+        public Keys Key { get; set; }
+        public KeyDownEventArgs(Keys key)
+        {
+            Key = key;
+        }
+    }
+}
---
+++ XNASingleStroke/SingleKeyStroke.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace XNASingleStroke
+{
+    public class SingleKeyStroke : GameComponent
+    {
+        public event EventHandler<KeyDownEventArgs> KeyDown = delegate { };
+
+        private int pressedKeys;
+        public SingleKeyStroke(Game game) : base(game) { }
+
+        public override void Update(GameTime gameTime)
+        {
+            var pressed = Keyboard.GetState().GetPressedKeys();
+            var keys = 0;
+            foreach (var key in pressed)
+            {
+                var num = 1 << (int)key;
+                keys |= num;
+                if ((pressedKeys & num) != num)
+                {
+                    KeyDown(this, new KeyDownEventArgs(key));
+                }
+            }
+            pressedKeys = keys;
+            base.Update(gameTime);
+        }
+    }
+}
